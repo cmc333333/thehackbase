a784e9c23c1a08035902bb18bd4a7ae813b4c0e0
CM Lubinski <cmc333333@gmail.com>
2011-04-29 11:57:23 -0500

    Start of templates

--- journalists/views.py
+++ journalists/views.py
@@ -1,1 +1,4 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+def newJournalist(request):
+  return render_to_response('journalists/new.xhtml', {'title': 'New Journalist'})
